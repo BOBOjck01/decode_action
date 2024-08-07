a170ce2630a6f8431258d69e10b8fe126863f912
action <action@github.com>
2024-08-07 07:07:56 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Mon Aug 05 2024 02:21:20 GMT+0000 (Coordinated Universal Time)
+//Wed Aug 07 2024 07:07:56 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const _0x1b3bab = $.isNode() ? require("./jdCookie") : "",
